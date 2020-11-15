401c13b7513b6c93fce0b2d56b93457c4f80058f
Daniel Winsvold <danielwinsvold@gmail.com>
2020-11-15 20:25:27 +0100

    trekker ut kalender i egen komponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import styled from "styled-components/macro";
-import { GlobalStyles, theme } from "./theme";
-import Luke from "./Components/Luke";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { GlobalStyles } from "./theme";
 import LukeWrapper from "./Components/LukeWrapper";
-
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(${theme.lukeSize}, 1fr));
-  grid-row-gap: 3rem;
-  grid-column-gap: 1rem;
-  padding: 1rem 2rem 10rem;
-  max-width: 60rem;
-  margin: 0 auto;
-`;
-
-const Header = styled.header`
-  text-align: center;
-  margin: 1.5rem auto 4rem;
-  h1 {
-    font-size: 5rem;
-    margin-bottom: 1rem;
-  }
-
-  span {
-    font-size: 1.8rem;
-    font-family: "Lobster Two", cursive;
-    letter-spacing: 0.15em;
-  }
-`;
+import Kalender from "./Components/Kalender";
 
 function App() {
   return (
@@ -37,15 +11,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/">
-            <Header>
-              <h1>Julekalender</h1>
-              <span>🎅 for stillesittende kontor-nisser 🎅</span>
-            </Header>
-            <Wrapper>
-              {[...Array(24)].map((item, index) => (
-                <Luke key={index} nummer={index + 1} />
-              ))}
-            </Wrapper>
+            <Kalender />
           </Route>
           <Route path="/:lukeNummer">
             <LukeWrapper />
---
+++ src/Components/Kalender.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Luke from "./Luke";
+import styled from "styled-components/macro";
+import { theme } from "../theme";
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(${theme.lukeSize}, 1fr));
+  grid-row-gap: 3rem;
+  grid-column-gap: 1rem;
+  padding: 1rem 2rem 10rem;
+  max-width: 60rem;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  text-align: center;
+  margin: 1.5rem auto 4rem;
+  h1 {
+    font-size: 5rem;
+    margin-bottom: 1rem;
+  }
+
+  span {
+    font-size: 1.8rem;
+    font-family: "Lobster Two", cursive;
+    letter-spacing: 0.15em;
+  }
+`;
+
+function Kalender() {
+  return (
+    <div>
+      <Header>
+        <h1>Julekalender</h1>
+        <span>🎅 for stillesittende kontor-nisser 🎅</span>
+      </Header>
+      <Wrapper>
+        {[...Array(24)].map((item, index) => (
+          <Luke key={index} nummer={index + 1} />
+        ))}
+      </Wrapper>
+    </div>
+  );
+}
+
+export default Kalender;
